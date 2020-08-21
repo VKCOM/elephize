0586be34d73f1eb72ab36dc22404abc6f8492370
Oleg Klimenko <o.klimenko@2gis.ru>
2020-08-21 21:18:25 +0300

    Update prepack script

--- external/prepack.sh
+++ external/prepack.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sed -i '.bak' 's/"main": "src\/cli.ts"/"main": "dist\/src\/cli.js"/g' package.json
+sed -i.bak -e 's/"main": "src\/cli.ts"/"main": "dist\/src\/cli.js"/g' package.json
