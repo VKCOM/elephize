df6ec813e1e071c5190512108cc0294a4c927689
Egor Smirnov <esmirnov.xyz@gmail.com>
2021-04-30 14:49:10 +0300

    fix

--- src/ts2php/components/cjsModules/commonjsExternalModule.ts
+++ src/ts2php/components/cjsModules/commonjsExternalModule.ts
@@ -57,7 +57,7 @@ namespace ${this._namespaces.root}\\${fullyQualifiedNamespace};
 use ${this._namespaces.builtins}\\Stdlib;
 use ${this._namespaces.builtins}\\CJSModule;
 
-require __DIR__ . '/${this._normalizeRelativePath(this._implPath, this.targetFileName)}';
+require_once __DIR__ . '/${this._normalizeRelativePath(this._implPath, this.targetFileName)}';
 
 class ${this.className} extends CJSModule {
   /**
