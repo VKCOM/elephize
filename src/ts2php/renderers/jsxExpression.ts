4d2bb16cb2ce328b60323326e6014b7d13e8bb3e
Oleg Klimenko <me@ctizen.dev>
2021-06-04 14:24:15 +0300

    Extract shouldEscape func

--- src/ts2php/renderers/jsxExpression.ts
+++ src/ts2php/renderers/jsxExpression.ts
@@ -4,22 +4,33 @@ import { Context } from '../components/context';
 import { renderNode } from '../components/codegen/renderNodes';
 
 export const tJsxExpression = (node: ts.JsxExpression, context: Context<Declaration>) => {
-  if (node.expression &&
-    !ts.isLiteralExpression(node.expression) &&
-    !ts.isNumericLiteral(node.expression) &&
-    !ts.isStringLiteral(node.expression) &&
-    node.expression.kind !== ts.SyntaxKind.TrueKeyword &&
-    node.expression.kind !== ts.SyntaxKind.FalseKeyword
-  ) {
+  if (shouldEscape(node.expression, context)) {
     return `\\${context.namespaces.builtins}\\IntrinsicElement::escape(` + renderNode(node.expression, context) + ')';
   }
+  return renderNode(node.expression, context);
+};
 
-  /*
+// Check if jsx expression should be html-escaped while rendering on server side
+function shouldEscape(node: ts.Expression | undefined, context: Context<Declaration>) {
+  if (!node) {
+    return false;
+  }
+
+  if (ts.isLiteralExpression(node) || ts.isNumericLiteral(node) || ts.isStringLiteral(node)) {
+    return false;
+  }
+
+  if (node.kind === ts.SyntaxKind.TrueKeyword || node.kind === ts.SyntaxKind.FalseKeyword) {
+    return false;
+  }
+
+  return true;
+}
+
+/*
   TODO
   Текущие проблемы:
   - Двойное экранирование для вложенных конструкций типа <Outer>{...map(() => <Cmp>{smth}</Cmp>)}</Outer>
   - Экранирование children - <Outer>{children}</Outer> - излишне.
   - Внутри строго текстовых нод не раскукоживаются utf-символы
    */
-  return renderNode(node.expression, context);
-};
