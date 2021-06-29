c5c14b651cc605609b2e6662438674e6b0db036e
Egor Smirnov <e.smirnov@corp.vk.com>
2021-06-29 18:30:04 +0300

    add @elephizeTarget attr to ReexportResolve component

--- src/__tests__/specimens/components/ReexportResolve.tsx
+++ src/__tests__/specimens/components/ReexportResolve.tsx
@@ -2,8 +2,11 @@ import { getFoo, SOME_CONST } from "./PathResolve/index";
 import { ReactPathResolveComponent } from "./PathResolveReact/index";
 
 getFoo();
+console.log(SOME_CONST);
 
+// @elephizeTarget
 function render() {
+  getFoo();
   console.log(SOME_CONST);
   return <ReactPathResolveComponent></ReactPathResolveComponent>;
 }
