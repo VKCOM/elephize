e914f3e6fca52fa04ac0a9d4ce32ade0781aed74
Oleg Klimenko <me@ctizen.dev>
2021-11-10 18:19:56 +0300

    Add kphp debug folder

--- src/builtins/ReactContext.php
+++ src/builtins/ReactContext.php
@@ -10,7 +10,7 @@ class ReactContext {
     /**
      * @var mixed
      */
-    private $_lastNotNullValue = null;
+    private $_lastNotNullValue;
 
     /**
      * @param mixed $initialValue
@@ -33,7 +33,7 @@ class ReactContext {
      * @param ?mixed $providerValue
      * @return null
      */
-    public static function pushContext(ReactContext $ctx, $providerValue = null) {
+    public static function pushContext(ReactContext &$ctx, $providerValue = null) {
         $ctx->_valuesStack []= $providerValue;
         if ($providerValue !== null) {
             $ctx->_lastNotNullValue = $providerValue;
@@ -45,9 +45,10 @@ class ReactContext {
      * @param ReactContext $ctx
      * @return null
      */
-    public static function popContext(ReactContext $ctx) {
+    public static function popContext(ReactContext &$ctx) {
         $val = array_pop($ctx->_valuesStack);
         if ($val !== null) {
+            $ctx->_lastNotNullValue = null;
             for ($i = count($ctx->_valuesStack) - 1; $i >= 0; $i--) {
                 if ($ctx->_valuesStack[$i] !== null) {
                     $ctx->_lastNotNullValue = $ctx->_valuesStack[$i];
@@ -70,7 +71,7 @@ class ReactContext {
      * @param ReactContext $ctx
      * @return mixed
      */
-    public static function getValue(ReactContext $ctx) {
+    public static function getValue(ReactContext &$ctx) {
         return $ctx->_lastNotNullValue;
     }
 }
