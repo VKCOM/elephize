34e282721614ea9ef343e57d6db7d451e7e21434
Vadim Leven <v.leven@corp.vk.com>
2022-01-31 21:46:07 +0300

    support useRef hook

--- src/ts2php/components/react/reactHooks.ts
+++ src/ts2php/components/react/reactHooks.ts
@@ -63,8 +63,12 @@ const hookRenderers: HookRenderers = {
     return renderNode(node.arguments[0], context);
   },
 
+  'useRef': (node, context) => {
+    const val = renderNode(node.arguments[0], context); // recognize only 1st argument of call
+    return `["current" => ${val}]`;
+  },
+
   'useCallback': dropRender,
-  'useRef': dropRender,
 
   'useEffect': () => '!null',
   'useImperativeHandle': () => '!null',
